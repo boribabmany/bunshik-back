074aed6e2a40cdb924ea17ac1834de78b6d1578b
pyouji99@naver.com <parkbori99@gmail.com>
2026-07-22 12:14:05 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/bunshik/bunshikback/BunshikBackApplication.java
@@ -0,0 +1,13 @@
+package com.bunshik.bunshikback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunshikBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BunshikBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bunshik/bunshikback/BunshikBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunshik.bunshikback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunshikBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
