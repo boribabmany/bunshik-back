562af2e468e68182552f7ea878c90e87414a0909
나다 <kmj07270@naver.com>
2026-07-23 10:20:47 +0900

    CorsConfig 추가

--- src/main/java/com/bunshik/common/config/CorsConfig.java
+++ src/main/java/com/bunshik/common/config/CorsConfig.java
@@ -1,4 +1,22 @@
 package com.bunshik.common.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:3000",   // kiosk-customer
+                        "http://localhost:3001"    // kiosk-admin
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
