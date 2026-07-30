685e5cd012af88be58be75992543e6eaa5ae425b
pyouji99@naver.com <parkbori99@gmail.com>
2026-07-30 14:16:12 +0900

    옵션수량 2개 최대선택 로직 추가

--- src/main/java/com/bunshik/kiosk/dto/OrderItemDto.java
+++ src/main/java/com/bunshik/kiosk/dto/OrderItemDto.java
@@ -3,6 +3,7 @@ package com.bunshik.kiosk.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class OrderItemDto {
     @Min(value = 1, message = "수량은 1개 이상이어야 합니다.")
     private Integer quantity;
 
+    @Size(max = 2, message = "옵션은 최대 2개까지 선택할 수 있습니다.")
     @JsonProperty("option_ids")
     private List<Integer> optionIds;
 }
